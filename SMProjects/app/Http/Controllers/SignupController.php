105c516b4bb4274612087c8016c61f4aff61bce7
TienDoan13 <105089710+TienDoan13@users.noreply.github.com>
2022-07-30 14:35:33 +0700

    Test
    test

--- SMProjects/app/Http/Controllers/SignupController.php
+++ SMProjects/app/Http/Controllers/SignupController.php
@@ -11,4 +11,14 @@ class SignupController extends Controller
     public function signup(){
         return view('client.page.indexsignup');
     }
+
+    public function postSignup(Request $request)
+    {
+        $user = new User;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->save();
+        return redirect()->route('client.page.signin')->with('Succesfully', 'Signup Succesfully');
+    }
 }
