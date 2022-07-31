8c31b6fee1617c727807b997b0e12c0f6fb3d7b0
Nhatqqq <105089693+Nhatqqq@users.noreply.github.com>
2022-07-31 16:20:42 +0700

    signin signup
    chinh sửa controller

--- SMProjects/app/Http/Controllers/SignupController.php
+++ SMProjects/app/Http/Controllers/SignupController.php
@@ -10,7 +10,7 @@ use App\Models\User;
 class SignupController extends Controller
 {
     public function getSignup(){
-        return view('client.page.indexsignup');
+        return view('client.page.signup');
     }
 
     public function postSignup(Request $request)
