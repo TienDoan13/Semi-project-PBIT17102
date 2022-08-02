9990b45a6da3d83b89bcd5083068b4be1ab42c81
Nhatqqq <105089693+Nhatqqq@users.noreply.github.com>
2022-08-02 21:46:39 +0700

    Fix lỗi đăng nhập
    ok rồi nha có lỗi gì ae báo nha

--- SMProjects/app/Models/Homepage.php
+++ SMProjects/app/Models/Homepage.php
@@ -11,5 +11,9 @@ class Homepage extends Model
     protected $table='movie';
     public $primaryKey='movie_id';
     public $timestamps=false;
-    protected $filltable=['movie_name','movie_description','release_date','director','main_character','price','movie_img'];
+    protected $filltable=['movie_name','movie_description','release_date','director','main_character','price','movie_img','category_id'];
+
+    public function category(){
+        return $this->belongsTo('App\Models\Category','category_id','category_id');
+    }
 }
